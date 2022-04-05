952cfbeb6ccbc080f561840fb06c51a5873d5128
on7011 <nao.ohashi@lifebox.co.jp>
2022-04-05 17:02:27 +0900

    sub1

--- src/main/java/com/example/demo/Sample1.java
+++ src/main/java/com/example/demo/Sample1.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 public class Sample1 {
 	public static void main(String[] args) {
+		
+		
         System.out.println("Main Branch."); //←ここがsub1ブランチと異なる
         System.out.println("Sub Branch.");
     }

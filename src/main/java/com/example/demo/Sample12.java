bb4b20fbc67f481e679eb20f34321a0aa4643f27
on7011 <nao.ohashi@lifebox.co.jp>
2022-04-05 15:10:45 +0900

    aaa

---
+++ src/main/java/com/example/demo/Sample12.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Sample12{
+    public static void main(String[] args) {
+        System.out.println("12-1.");
+        System.out.println("12-1. @master.");
+    }
+}
--- src/main/java/com/example/demo/Sample2.java
+++ src/main/java/com/example/demo/Sample2.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 public class Sample2 {
 	public static void main(String[] args) {
-		   System.out.println("Sample11.");
-	        System.out.println("Sample11. sub1."); //←1度目の修正コミットで追加
-	        System.out.println("Sample11. sub1."); //←2度目の修正コミットで追加
-	        }
+
+		 System.out.println("Sample11.");
+	        System.out.println("Sample11. master."); //←修正コミットで追加
+
+    }
+
 }

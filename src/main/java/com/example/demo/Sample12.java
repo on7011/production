56824a18259ba8a172f1684be675fca5d86664be
on7011 <nao.ohashi@lifebox.co.jp>
2022-04-05 17:04:21 +0900

    main12

--- src/main/java/com/example/demo/Sample12.java
+++ src/main/java/com/example/demo/Sample12.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 public class Sample12{
     public static void main(String[] args) {
+    	
+    	
+    	
         System.out.println("12-1.");
         System.out.println("12-1. @master.");
     }

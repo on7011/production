15525390467ab90afc387e9d733bea91a22ebf46
on7011 <nao.ohashi@lifebox.co.jp>
2022-04-05 17:03:32 +0900

    main1

--- src/main/java/com/example/demo/Sample2.java
+++ src/main/java/com/example/demo/Sample2.java
@@ -3,6 +3,8 @@ package com.example.demo;
 public class Sample2 {
 	public static void main(String[] args) {
 
+		
+		
 		 System.out.println("Sample11.");
 	        System.out.println("Sample11. master."); //←修正コミットで追加
 
